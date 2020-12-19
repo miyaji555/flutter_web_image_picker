7a446da510a6190d309302ad22228baaebdf58b8
miyaji555 <kou.sepak@gmail.com>
2020-12-19 14:16:13 +0900

    ネットワーク画像を表示できた。

--- lib/image_picker/image_picker_page.dart
+++ lib/image_picker/image_picker_page.dart
@@ -12,16 +12,25 @@ class ImagePickerPage extends StatelessWidget {
         return Consumer<ImagePickerModel>(builder: (context, model, child) {
           return Scaffold(
             body: Container(
+              color: Colors.black12,
               height: double.infinity,
               width: double.infinity,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text(model.textMessage),
-                  Center(
-                      child: RaisedButton(
-                    onPressed: model.fetch,
-                  )),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    padding: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                              'https://1.bp.blogspot.com/-kgcAHrla1MM/WkR96Ewyx2I/AAAAAAABJWA/feKfyDf44J4xQObtAu8WXWsxT8Osk-ucQCLcBGAs/s800/oomisoka_yoiotoshio_summer_woman.png')),
+                    ),
+                  ),
                 ],
               ),
             ),
